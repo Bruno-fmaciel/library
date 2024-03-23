61e85d46411fcfdecf00b5583d2b940317d914be
Bruno-fmaciel <bfma@cesar.school>
2024-03-23 13:37:42 -0300

    Add toggle and remove button functions

--- script.js
+++ script.js
@@ -7,52 +7,79 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead();
+    addBookCard();
+}
+
 function addBook() {
     if (checkbox.checked) {
         let title = document.querySelector("#title").value;
         let author = document.querySelector("#author").value;
         let pages = document.querySelector("#pages").value;
-        let book = new Book(title, author, pages, "Read");
+        let book = new Book(title, author, pages, read);
         myLibrary.push(book);
         addBookCard();
     } else {
         let title = document.querySelector("#title").value;
         let author = document.querySelector("#author").value;
         let pages = document.querySelector("#pages").value;
-        let book = new Book(title, author, pages, "Not read");
+        let book = new Book(title, author, pages, !read);
         myLibrary.push(book);
         addBookCard();
     }
 }
 
+
+
 function addBookCard() {
     const container = document.querySelector(".books");
-    const div = document.createElement("div");
-    const h2 = document.createElement("h2");
-    const p1 = document.createElement("p");
-    const p2 = document.createElement("p");
-    const p3 = document.createElement("p");
-    const div2 =  document.createElement("div");
-    const btn1 = document.createElement('button');
-    const btn2 = document.createElement('button');
+    container.innerHTML = "";
+    for (let i = 0 ; i < myLibrary.length ; i++) {
+        let book = myLibrary[i];
+        const div = document.createElement("div");
+        const h2 = document.createElement("h2");
+        h2.innerText = book.title;
+        const p1 = document.createElement("p");
+        p1.innerText = book.author;
+        const p2 = document.createElement("p");
+        p2.innerText = book.pages;
+        const p3 = document.createElement("p");
+        p3.innerText = book.read? "Read" : "Not read";
+        const div2 =  document.createElement("div");
+        const btn1 = document.createElement('button');
+        const btn2 = document.createElement('button');
 
-    div.classList.add("book");
-    container.appendChild(div);
-    div.appendChild(h2);
-    div.appendChild(p1);
-    div.appendChild(p2);
-    div.appendChild(p3);
-    div.appendChild(div2);
-    div2.appendChild(btn1);
-    div2.appendChild(btn2);
-    div2.classList.add("buttons");
-    btn1.setAttribute("id","toggle");
-    btn1.innerText = "Toggle read";
-    btn2.classList.add("remove");
-    btn2.innerText = "Remove";
-    div.style["background: black"];
-
+        div.classList.add("book");
+        container.appendChild(div);
+        div.appendChild(h2);
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        p3.classList.add("read");
+        div.appendChild(div2);
+        div2.appendChild(btn1);
+        div2.appendChild(btn2);
+        div2.classList.add("buttons");
+        btn1.setAttribute("id","toggle");
+        btn1.innerText = "Toggle read";
+        btn1.onclick = function() {
+            myLibrary[i].toggleRead();
+            addBookCard();
+        }
+        btn2.classList.add("remove");
+        btn2.innerText = "Remove";
+        btn2.onclick = function(i) {myLibrary.splice(i,1);
+        addBookCard()};
+        div.style["background: black"];
+    }
 }
+
+
 
 let showForm = document.querySelector(".form");
 showForm.addEventListener('click', () => {
@@ -69,6 +96,8 @@ addNewBook.addEventListener('click',(e) => {
     e.preventDefault();
     addBook();
 });
+
+
     
 
 
